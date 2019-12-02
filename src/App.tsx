c5700ad8261382be00e6ee53731ca09cfa2a3462
Sehyun Chung <sxhyxnchxng@gmail.com>
2019-12-03 07:59:09 +0900

    Add basic view logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Bpm } from './core/tapper'
+
+const bipi = new Bpm(5)
 
 const App: React.FC = () => {
-  return <div className="App"></div>
+  const [bpm, setBpm] = useState(0)
+  const tap = () => {
+    bipi.tap()
+    if (bipi.bpm) setBpm(bipi.bpm)
+  }
+  return (
+    <div className="App">
+      <div>{bpm}</div>
+      <button onClick={tap}>tap!</button>
+    </div>
+  )
 }
 
 export default App
