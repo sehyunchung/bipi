47f506009b57c16d274b585bf53aa024b474e87a
Sehyun Chung <sxhyxnchxng@gmail.com>
2019-12-03 22:28:58 +0900

    Split components
    - Add Header
    - Add useBpm custom hook
    - Split Tapper component from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import React, { useState } from 'react'
 import { Bpm } from './core/tapper'
 
-const bipi = new Bpm(5)
-
-const App: React.FC = () => {
+const useBpm = () => {
+  const bipi = new Bpm()
   const [bpm, setBpm] = useState(0)
+
   const tap = () => {
     bipi.tap()
     if (bipi.bpm) setBpm(bipi.bpm)
   }
+
+  return { bpm, tap }
+}
+
+const Tapper: React.FC = () => {
+  const { bpm, tap } = useBpm()
+
   return (
-    <div className="App">
+    <div className="tapper">
       <div>{bpm}</div>
       <button onClick={tap}>tap!</button>
+    </div>
+  )
+}
+
+const Header = () => <h1>Bipi!</h1>
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Tapper />
     </div>
   )
 }
