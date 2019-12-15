47f9238b7915e27d5ef1865186339253ff528c25
Sehyun Chung <sxhyxnchxng@gmail.com>
2019-12-15 20:39:01 +0900

    Fix tapper test file and add a test case

--- src/core/tapper.test.ts
+++ src/core/tapper.test.ts
@@ -1,28 +1,41 @@
-import { Bpm } from './tapper'
+import { Tapper } from './tapper'
 
-describe('Bpm 클래스를 테스트하자', () => {
-  let tapper: Bpm
+describe('Tapper 클래스를 테스트하자', () => {
+  let tapper: Tapper
 
   beforeEach(() => {
-    tapper = new Bpm()
+    tapper = new Tapper()
   })
 
-  it('tapper.tap()을 실행하지 않았을 때 bpm은 0이다.', () => {
+  test('tapper.tap()을 실행하지 않았을 때 bpm은 0이다.', () => {
     expect(tapper.bpm).toBe(0)
   })
 
-  it('500ms 간격으로 4번 탭하면 bpm은 120이다. 그 후 reset()을 호출하면 bpm은 0이다.', async () => {
-    const tap120 = new Promise(res => {
-      let id = setInterval(() => {
+  test('500ms 간격으로 4번 탭하면 bpm은 120이다. 그 후 reset()을 호출하면 bpm은 0이다.', async () => {
+    const tap120 = new Promise(resolve => {
+      const id = setInterval(() => {
         tapper.tap()
       }, 500)
       setTimeout(() => {
-        res(clearInterval(id))
+        resolve(clearInterval(id))
       }, 2000)
     })
     await tap120
     expect(tapper.bpm).toBe(120)
     tapper.reset()
     expect(tapper.bpm).toBe(0)
+  })
+
+  test('400ms 간격으로 4번 탭하면 bpm은 150이다.', async () => {
+    const tap150 = new Promise(resolve => {
+      const id = setInterval(() => {
+        tapper.tap()
+      }, 400)
+      setTimeout(() => {
+        resolve(clearInterval(id))
+      }, 1600)
+    })
+    await tap150
+    expect(tapper.bpm).toBe(150)
   })
 })
