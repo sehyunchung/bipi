89b17afcb585538cfc0e47129893a733534e4c75
Sehyun Chung <sxhyxnchxng@gmail.com>
2019-12-24 17:15:42 +0900

    Change tappable area

--- src/components/Bpm.tsx
+++ src/components/Bpm.tsx
@@ -1,23 +1,20 @@
 /** @jsx jsx */
-import React, { Fragment, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { css, jsx } from '@emotion/core'
-import { MobileView, BrowserView } from 'react-device-detect'
 import { Tapper } from '../core/tapper'
 
 const bpmStyle = css`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translateX(-50%) translateY(-50%);
   width: 100%;
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   user-select: none;
   font-size: 50vw;
-  font-family: 'IBM Plex Mono', Menlo, 'Fira Code', Inconsolata, Monaco,
-    Consolas, 'Courier New', Courier, monospace;
+  font-family: 'IBM Plex Mono', Menlo, Inconsolata, Monaco, Consolas,
+    'Courier New', Courier, monospace;
   text-align: center;
   color: #ffffff;
-  background: palegoldenrod;
 `
 
 const useTapper = () => {
@@ -38,30 +35,21 @@ const useTapper = () => {
 }
 
 const Bpm: React.FC = () => {
-  const tapper = useTapper()
+  const { bpm, tap, reset } = useTapper()
 
   useEffect(() => {
     const id = setTimeout(() => {
-      tapper.reset()
+      reset()
     }, 3000)
     return () => {
       clearTimeout(id)
     }
-  }, [tapper.bpm])
+  }, [bpm, reset])
 
   return (
-    <Fragment>
-      <MobileView>
-        <div css={bpmStyle} onTouchEnd={tapper.tap}>
-          {tapper.bpm}
-        </div>
-      </MobileView>
-      <BrowserView>
-        <div css={bpmStyle} onClick={tapper.tap}>
-          {tapper.bpm}
-        </div>
-      </BrowserView>
-    </Fragment>
+    <div css={bpmStyle} onClick={tap}>
+      <span>{bpm}</span>
+    </div>
   )
 }
 
