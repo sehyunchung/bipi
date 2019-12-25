a43e47debf49e5646a85e894b824f0c1c6f829cb
Sehyun Chung <sxhyxnchxng@gmail.com>
2019-12-25 14:13:17 +0900

    Change animation timing and intensity

--- src/components/Bpm.tsx
+++ src/components/Bpm.tsx
@@ -41,7 +41,7 @@ const Bpm: React.FC = () => {
   const { x } = useSpring({
     from: { x: 0 },
     x: on ? 1 : 0,
-    config: { duration: 100 },
+    config: { duration: 80 },
   })
 
   const handleClick = () => {
@@ -65,7 +65,7 @@ const Bpm: React.FC = () => {
         style={{
           transform: x
             .interpolate({
-              range: [0, 0.5, 1],
+              range: [0, 0.4, 1],
               output: [1, 0.8, 1],
             })
             .interpolate(x => `scale(${x})`),
