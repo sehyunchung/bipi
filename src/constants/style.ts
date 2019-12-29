8c5571e43f2b07d0571ada98686e7c23ac72ddf1
Sehyun Chung <sxhyxnchxng@gmail.com>
2019-12-29 16:12:42 +0900

    Change colors

--- src/constants/style.ts
+++ src/constants/style.ts
@@ -1,7 +1,7 @@
-const BACKGROUND: string = 'hsl(0, 0%, 66%)'
-const BPM_PRIMARY_TEXT: string = 'hsl(0, 0%, 94%)'
-const BPM_SECONDARY_TEXT: string = 'hsl(0, 0%, 84%)'
-const FOOTER_TEXT: string = 'hsl(0, 0%, 76%)'
+const BACKGROUND: string = 'hsl(0, 0%, 64%)'
+const BPM_PRIMARY_TEXT: string = 'hsl(0, 0%, 86%)'
+const BPM_SECONDARY_TEXT: string = 'hsl(0, 0%, 76%)'
+const FOOTER_TEXT: string = 'hsl(0, 0%, 72%)'
 
 const FAMILY: string = `'IBM Plex Mono', Menlo, Inconsolata, Monaco, Consolas,
     'Courier New', Courier, monospace`
