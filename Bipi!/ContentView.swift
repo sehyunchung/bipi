cdceb4742519e2848315ab8f1bc9efa5ec3c578e
Sehyun Chung <sxhyxnchxng@gmail.com>
2020-01-08 17:32:38 +0900

    Initial Commit

---
+++ Bipi!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bipi!
+//
+//  Created by Se Hyun Chung on 2020/01/08.
+//  Copyright © 2020 Sehyun Chung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Bipi!Tests/Bipi_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Bipi_Tests.swift
+//  Bipi!Tests
+//
+//  Created by Se Hyun Chung on 2020/01/08.
+//  Copyright © 2020 Sehyun Chung. All rights reserved.
+//
+
+import XCTest
+@testable import Bipi_
+
+class Bipi_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
