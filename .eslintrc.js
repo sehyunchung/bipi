357d15bb2fb5779f9fc264e727e02b34e2e2d2d7
Sehyun Chung <sehyun.chung@icloud.com>
2021-02-14 00:00:23 +0900

    chore: migrate configs to react-native-web

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,17 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  extends: ['plugin:@typescript-eslint/recommended'],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+  ],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: { jsx: true },
   },
-  plugins: ['react/recommended', 'react-hooks'],
+  plugins: ["react", "react-hooks"],
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
   },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: 'detect',
-    },
-  },
-}
+};
